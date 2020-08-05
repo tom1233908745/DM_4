77c8f555f8f67da80eb78e73b2478367e28d80c4
s1270246 <67380619+tom1233908745@users.noreply.github.com>
2020-08-05 12:46:08 +0900

    HoT: Initial revision

---
+++ ex4.py
@@ -0,0 +1,29 @@
+import random
+def main():
+
+
+    heads = 0
+    tails = 0
+
+
+    # コイントスパート
+    print("Tossing a coin...");
+
+    for i in range(3):
+        num = random.random()
+        print(num)
+        if num*10 >= 5:
+            print("Round " + str(i + 1) + ": Head")
+            heads += 1
+        else:
+
+            print("Round " + str(i + 1) + ": Tail")
+            tails += 1
+
+    print("Heads: " + str(heads) + " Tails:" + str(tails))
+   
+
+    return
+
+if __name__ == "__main__":
+    main()
